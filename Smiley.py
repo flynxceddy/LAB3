bdb5d22a75472c00bfcf1a93c3c5102528893945
cedric1332 <bryan.cedric.pedragoza@gmail.com>
2021-05-22 19:35:16 +0800

    Version 1 by BryanCedricPedragoza

---
+++ Smiley.py
@@ -0,0 +1,7 @@
+print ("XXXXXXXXXXXXX'")
+print ("X  _    _   X'")
+print ("X  0    0   X'")
+print ("X     ^     X'")
+print ("X \ \____/  >'")
+print ("X           X'")
+print ("XXXXXXXXXXXXX'")
---
+++ inform.py
@@ -0,0 +1,4 @@
+print ("Name: Pedragoza, Bryan Cedric P.")
+print ("Course & Section: BSIT SFIT2A")
+print ("Contact: 09153213091")
+print ("E-Mail: bryan.cedric.pedragoza@gmail.com")
